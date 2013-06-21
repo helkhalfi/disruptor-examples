9cd9536dd13005121bce6fcfb3b2c32f7a31a516
Hichame EL KHALFI <hichame.elkhalfi@gmail.com>
2013-06-21 09:00:20 +0200

    first commit code

---
+++ src/main/java/com/mcba/disruptor/MyHandler.java
@@ -0,0 +1,29 @@
+package com.mcba.disruptor;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.lmax.disruptor.EventHandler;
+
+public final class MyHandler implements EventHandler<ValueEvent>
+{
+	private final long ordinal;
+	private final long numberOfConsumers;
+	private AtomicBoolean hasBeenConsumed = new AtomicBoolean(false);
+
+	public MyHandler(final long ordinal, final long numberOfConsumers)
+	{
+		this.ordinal = ordinal;
+		this.numberOfConsumers = numberOfConsumers;
+	}
+
+	@Override
+	public void onEvent(final ValueEvent entry, final long sequence, final boolean onEndOfBatch)
+	{
+		//if ((sequence % numberOfConsumers) == ordinal)
+		if(hasBeenConsumed.get())
+		{
+			System.out.println("Sequence: [" + sequence + "], numberOfConsumers["+numberOfConsumers+"], ordinal["+ordinal+"]");
+
+		}
+	}
+}
